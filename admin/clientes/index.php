ab53470302e275c61de321d9cade19a52bf67c95
lucas64987 <134726721+lucas64987@users.noreply.github.com>
2023-10-23 17:04:31 -0300

    atualizacao

---
+++ admin/clientes/index.php
@@ -0,0 +1,51 @@
+<?php
+require_once $_SERVER['DOCUMENT_ROOT']. "/includes/cabecalho.php";
+?>
+<!DOCTYPE html>
+<html>
+<head>
+    <title>logo infotec</title>
+    <img src="img/logo-infotec.png" alt="paisagem" width="200">
+    <title>sobre nós</title>
+    <style>
+        .centro {
+            text-align: center;
+        }
+
+        h1 {
+            color: #001e8d; /* Define a cor de texto como #001e8d (azul escuro) */
+            font-size: 24px; /* Aumenta o tamanho das letras do cabeçalho para 24 pixels */
+        }
+
+        p {
+            color: black; /* Define a cor do texto do parágrafo como branco */
+            font-size: 18px; /* Aumenta o tamanho das letras do parágrafo para 18 pixels */
+        }
+    </style>
+</head>
+<body>
+    <div class="centro">
+        <h1>sobre nós da INFOTEC</h1>
+        <p>Bem-vindo à INFOTEC nossa
+        loja de hardware e periféricos! 
+        Somos uma equipe
+        apaixonada por tecnologia, 
+        e nossa missão é oferecer a 
+        você a melhor experiência 
+        em compras para atender
+        às suas necessidades 
+        de hardware e periféricos.
+
+        Na na INFOTEC, você encontrará 
+        uma ampla variedade de produtos para 
+        atender às suas demandas, seja você 
+        um entusiasta de jogos procurando por 
+        placas de vídeo poderosas, um profissional 
+        que precisa de componentes confiáveis 
+        para montar uma estação de trabalho 
+        robusta, ou alguém que busca aprimorar 
+        sua experiência de computação com os 
+        melhores periféricos do mercado.</p>
+    </div>
+</body>
+</html>
