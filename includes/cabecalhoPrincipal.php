213d3d1677d5ef74803b389258bb0dd2d93781d9
lucas64987 <134726721+lucas64987@users.noreply.github.com>
2023-10-24 17:10:06 -0300

    novo

---
+++ includes/cabecalhoPrincipal.php
@@ -0,0 +1,17 @@
+<html>
+ 
+<head>
+    <style>
+        a {
+            color: #ffffff;
+        }
+    </style>
+</head>
+<footer class="bg-primary text-white text-center p-1 mt-auto">
+    <a href="sobreNos.php">SOBRE NÓS</a>
+    /
+    <a href="faleComNosco.php">FALE COM NOSCO</a>
+    <img src="/includes/img/fone.png" alt="Fone de ouvido">
+ 
+</footer>
+</html>
