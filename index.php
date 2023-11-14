060ececa92f79f5f04da300c07e9a522cea46b85
lucas64987 <134726721+lucas64987@users.noreply.github.com>
2023-11-14 17:14:31 -0300

    carroceu

--- index.php
+++ index.php
@@ -16,12 +16,40 @@
 </head>
 
 <body>
-    <title>INFOTEC</title>
+
+<title>INFOTEC</title>
+<style>
+  /* Estilo para as imagens do carrossel */
+  #carouselExampleSlidesOnly img {
+    max-height: 300px; /* Defina a altura máxima desejada */
+    width: auto; /* Isso manterá a proporção correta */
+  }
+</style>
+
+<div id="carouselExampleInterval" class="carousel slide" data-bs-ride="carousel">
+  <div class="carousel-inner">
+    <div class="carousel-item active" data-bs-interval="10000">
+      <img src="/includes/img/tkimg.jpg" class="d-block w-100" alt="...">
+    </div>
+    <div class="carousel-item" data-bs-interval="2000">
+      <img src="/includes/img/tk2img.jpg" class="d-block w-100" alt="...">
+    </div>
+    <div class="carousel-item">
+      <img src="/includes/img/tk3img.jpg" class="d-block w-100" alt="...">
+    </div>
+  </div>
+  <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleInterval" data-bs-slide="prev">
+    <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+    <span class="visually-hidden">Previous</span>
+  </button>
+  <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleInterval" data-bs-slide="next">
+    <span class="carousel-control-next-icon" aria-hidden="true"></span>
+    <span class="visually-hidden">Next</span>
+  </button>
+</div>
     
     
 
-   
-  
 
 
     </body>
